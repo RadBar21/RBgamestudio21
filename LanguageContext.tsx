4d5413067c2afa0ff58d90eff95f104dfb235bb9
Radek Bartoníček <radekbartonicek@Radek--MacBook-Pro.local>
2025-12-18 09:25:10 +0100

    Zmena vychoziho jazyka na EN

--- LanguageContext.tsx
+++ LanguageContext.tsx
@@ -11,7 +11,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>('cs');
+  const [language, setLanguage] = useState<Language>('en');
 
   const value = {
     language,
