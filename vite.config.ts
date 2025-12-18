023f8f01dc60cfae33885c2b3e79d36b66612aa3
Radek Bartoníček <radekbartonicek@Radek--MacBook-Pro.local>
2025-12-18 08:23:01 +0100

    Oprava cest a pridani nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/', // Zde byla změna: vracíme zpět na '/'
+      base: '/',  // <--- TOTO JE NEJDŮLEŽITĚJŠÍ (musí být jen lomítko)
       server: {
         port: 3000,
         host: '0.0.0.0',
